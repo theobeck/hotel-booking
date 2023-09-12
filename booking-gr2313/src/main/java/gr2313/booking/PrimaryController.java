c8473023af8b9ccadd45dfae588ad865c602971e
ktbeck <ktbeck@stud.ntnu.no>
2023-09-12 15:23:33 +0200

    added datepickers in PrimaryController.java

--- booking-gr2313/src/main/java/gr2313/booking/PrimaryController.java
+++ booking-gr2313/src/main/java/gr2313/booking/PrimaryController.java
@@ -2,6 +2,7 @@ package gr2313.booking;
 
 import java.io.IOException;
 import javafx.fxml.FXML;
+import javafx.scene.control.DatePicker;
 
 public class PrimaryController {
 
@@ -9,4 +10,7 @@ public class PrimaryController {
     private void switchToSecondary() throws IOException {
         App.setRoot("secondary");
     }
+
+    @FXML private DatePicker fromDate;
+    @FXML private DatePicker toDate;
 }
