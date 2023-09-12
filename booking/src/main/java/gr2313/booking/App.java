1388c0bf4b331a0bf6bc57c3ab0098fc496bece6
Ola Munthe Vassbotn <olamva@stud.ntnu.no>
2023-09-12 11:25:22 +0200

    Initialized Maven project

---
+++ booking/src/main/java/gr2313/booking/App.java
@@ -0,0 +1,13 @@
+package gr2313.booking;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
