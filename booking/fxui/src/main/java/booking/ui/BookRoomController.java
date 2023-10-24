2cbfa9fbc83024c83f8be5192d98c0b59e728284
Ola Munthe Vassbotn <olamva@stud.ntnu.no>
2023-10-24 13:52:41 +0200

    fix: satisfied checkstyle

--- booking/fxui/src/main/java/booking/ui/BookRoomController.java
+++ booking/fxui/src/main/java/booking/ui/BookRoomController.java
@@ -77,7 +77,7 @@ public class BookRoomController {
     /**
      * @param username Change the username of the user.
      */
-    public void setUsername(String username) {
+    public void setUsername(final String username) {
         this.username = username;
     }
 }
