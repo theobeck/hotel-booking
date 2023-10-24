ee548c1cb1e71711167854b84d88559aac183415
Ola Munthe Vassbotn <olamva@stud.ntnu.no>
2023-10-24 13:39:26 +0200

    fix: lara dumb

--- booking/fxui/src/test/java/booking/ui/BookingAppTest.java
+++ booking/fxui/src/test/java/booking/ui/BookingAppTest.java
@@ -8,7 +8,9 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.testfx.api.FxRobot;
 import org.testfx.framework.junit5.ApplicationTest;
-import org.testfx.matcher.base.NodeMatchers;
+
+import booking.core.Room;
+import booking.json.ReadWrite;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.ListView;
