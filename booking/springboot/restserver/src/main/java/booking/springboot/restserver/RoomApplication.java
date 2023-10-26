0c648d19fb331928ee454acc1bf4259a30b019a6
Ola Munthe Vassbotn <olamva@stud.ntnu.no>
2023-10-26 19:39:47 +0200

    fix: removed my bean
    co-authored by: @ktbeck

--- booking/springboot/restserver/src/main/java/booking/springboot/restserver/RoomApplication.java
+++ booking/springboot/restserver/src/main/java/booking/springboot/restserver/RoomApplication.java
@@ -1,22 +1,13 @@
 package booking.springboot.restserver;
 
-import java.util.Arrays;
-
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Bean;
 
 /**
  * The Spring application.
  */
 @SpringBootApplication
 public class RoomApplication {
-
-  // public RoomApplication() {
-  // throw new IllegalStateException("This class should not be instantiated.");
-  // }
 
   /**
    * The main method.
@@ -26,27 +17,4 @@ public class RoomApplication {
   public static void main(final String[] args) {
     SpringApplication.run(RoomApplication.class, args);
   }
-
-  /**
-   * A command line runner.
-   *
-   * @param ctx The application context
-   *
-   * @return A command line runner
-   */
-  @Bean
-  public CommandLineRunner commandLineRunner(final ApplicationContext ctx) {
-    return args -> {
-
-      System.out.println("Let's inspect the beans provided by Spring Boot:");
-
-      String[] beanNames = ctx.getBeanDefinitionNames();
-      Arrays.sort(beanNames);
-      for (String beanName : beanNames) {
-        System.out.println(beanName);
-      }
-
-    };
-  }
-
 }
